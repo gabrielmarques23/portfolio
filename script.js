95e10427859d675616cd4483f87fda0805a81219
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-04-16 22:09:45 -0300

    Função para rolagem de tela em JavaScript
    Adicionei a função para facilitar a XP do usuário.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function clickToUp(){
-    window.alert('OK OK OK')
+let clickBtnToTop =  document.querySelector('button.myBtnToUp');
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 550 || document.body.scrollTop > 550){
+        clickBtnToTop.style.display = "block";
+    } else{
+        clickBtnToTop.style.display = "none";
+    }
+
 }
 
 
+function scrollToTop(){
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
 
 
 
