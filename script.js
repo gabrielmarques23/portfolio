9980eaa725ac43d8d58879435b404e6920ab10c6
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-04-17 21:58:43 -0300

    Script provisório
    Por enquanto, porém irei fazer uma lógica melhor.

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-let clickBtnToTop =  document.querySelector('button.myBtnToUp');
 
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 550 || document.body.scrollTop > 550){
-        clickBtnToTop.style.display = "block";
-    } else{
-        clickBtnToTop.style.display = "none";
-    }
+let btnOpenNav = document.querySelector('button.btnHamb');
+let btnCloseNav = document.querySelector('button.btnClose');
 
+let headerNav = document.querySelector('.navHeaderDiv');
+
+function openNav() {
+    headerNav.classList.remove('hiddenHeaderNav');
+    headerNav.classList.add('.oppenHeaderNav');
+}
+function hiddenNav() {
+    headerNav.classList.add('hiddenHeaderNav');
+    headerNav.classList.remove('.oppenHeaderNav');
 }
 
+btnOpenNav.addEventListener("click", function () {
+    btnOpenNav.classList.add('changeNav');
+    btnCloseNav.classList.remove('changeNav');
+    openNav()
+});
 
-function scrollToTop(){
+btnCloseNav.addEventListener("click", function () {
+    btnOpenNav.classList.remove('changeNav');
+    btnCloseNav.classList.add('changeNav');
+    hiddenNav()
+});
+
+//Button to Scroll to Top
+let clickBtnToTop = document.querySelector('button.myBtnToUp');
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 550 || document.body.scrollTop > 550) {
+        clickBtnToTop.style.display = "block";
+    } else {
+        clickBtnToTop.style.display = "none";
+    }
+}
+
+function scrollToTop() {
     document.documentElement.scrollTop = 0;
     document.body.scrollTop = 0;
 }
+
 
 
 
